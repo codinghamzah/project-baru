2ea961b241cc5521d8c2e87c7c431c4b000287dd
wildan <>
2022-02-10 21:35:02 +0700

    Update Jumbotroncomponent.js

--- Jumbotroncomponent.js
+++ Jumbotroncomponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Button,
+  CardImg,
   Container,
   Modal,
   ModalBody,
@@ -20,7 +21,7 @@ const JumbotronComponent = (props) => {
       <div class="P-5 mb-4 bg-dark rounded-3 text-white">
         <div class="Container-fluid py-5">
           <h1 class="display-5 fw-bold">{props.title}</h1>
-          <p class="col-md-8 fs-4">Selamat Datang di website hana</p>
+          <p class="col-md-8 fs-4">Selamat Datang di website</p>
           <div>
             <Button color="danger" onClick={function noRefCheck() {}}>
               Click Me
